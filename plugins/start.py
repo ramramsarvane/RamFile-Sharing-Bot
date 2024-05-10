d9179cdd13260ff2f0ac22ae3460d4112d6c9cad
ramramsarvane <mehul410p@gmail.com>
2024-05-10 22:13:00 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -119,21 +119,24 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
     
     
     
-@Bot.on_message(filters.command('start') & filters.private)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import FORCE_SUB_CHANNELS
+
+@Bot.on_message(filters.command("start") & filters.private)
 async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink)
-        ]
-    ]
+    buttons = []
+    for channel_id in FORCE_SUB_CHANNELS:
+        invite_link = await client.export_chat_invite_link(channel_id)
+        buttons.append(
+            [InlineKeyboardButton("Join Channel", url=invite_link)]
+        )
+
     try:
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                    text='Try Again',
+                    url=f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
         )
@@ -141,18 +144,18 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
+        text=FORCE_MSG.format(
+            first=message.from_user.first_name,
+            last=message.from_user.last_name,
+            username=None if not message.from_user.username else '@' + message.from_user.username,
+            mention=message.from_user.mention,
+            id=message.from_user.id
+        ),
+        reply_markup=InlineKeyboardMarkup(buttons),
+        quote=True,
+        disable_web_page_preview=True
     )
-
+    
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
